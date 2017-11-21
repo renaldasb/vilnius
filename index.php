8aeecff96aa7467916abde579d9a08f2bcc414ae
renaldas <renaldas.bielskus@gmail.com>
2017-11-21 18:17:57 +0200

    pakeitimai

--- index.php
+++ index.php
@@ -1,11 +1,4 @@
 <?php
-/** Adminer - Compact database management
-* @link https://www.adminer.org/
-* @author Jakub Vrana, https://www.vrana.cz/
-* @copyright 2007 Jakub Vrana
-* @license http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
-* @license http://www.gnu.org/licenses/gpl-2.0.html GNU General Public License, version 2 (one or other)
-*/
 
 include "./include/bootstrap.inc.php";
 include "./include/tmpfile.inc.php";
